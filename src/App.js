4562851f0a088083add27f1e1417f97b7975cee1
belallbabidi <christian.lababidi@gmail.com>
2021-12-09 10:06:02 +0100

    asd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </header>
+     <h1> React Deploy1 </h1>     
+    </div>
+  );
+}
+
+export default App;
